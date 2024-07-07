cf0517b8ee6409b2536e1c23b72e27f33cde7867
Hung Truong <hungtruongquoc@gmail.com>
2024-07-07 13:00:06 -0500

    Added icon for links

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,6 +4,7 @@ import IntroductionItem from "@/components/introduction_item";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGithub} from "@fortawesome/free-brands-svg-icons/faGithub";
 import {faStar} from "@fortawesome/pro-solid-svg-icons/faStar";
+import {faArrowUpRightFromSquare} from "@fortawesome/pro-solid-svg-icons";
 
 export default function IndexPage() {
     return (
@@ -38,8 +39,9 @@ export default function IndexPage() {
                     <ul className="pt-5 px-10">
                         <span className="text-2xl"><FontAwesomeIcon icon={faGithub}/> Github</span>
                         <li>
-                            <a href="https://github.com/hungtruongquoc/nextjs-landing">
-                                This landing page was developed with NextJS, Tailwind CSS
+                            <a href="https://github.com/hungtruongquoc/nextjs-landing" target="_blank">
+                                This landing page was developed with NextJS, Tailwind CSS <FontAwesomeIcon
+                                icon={faArrowUpRightFromSquare}/>
                             </a>
                         </li>
                     </ul>
@@ -47,18 +49,34 @@ export default function IndexPage() {
                 <IntroductionItem title="Honorable Projects">
                     <ul className="pt-5 px-10">
                         <li>
-                            <a className="text-amber-400" href="https://beetrack.io/home-en/">
-                                Enterprise Level Asset Management Platform
+                            <a className="text-amber-400" href="https://beetrack.io/home-en/"
+                               target="_blank">
+                                Enterprise Level Asset Management Platform <FontAwesomeIcon
+                                icon={faArrowUpRightFromSquare}/>
                             </a>
                             <p>
                                 Customers
                             </p>
-                            <p className="pl-5"><a href="https://aeonmall.beetrack.vn/login">Aeon Mall Vietnam</a></p>
-                            <p className="pl-5"><a href="https://movenpickpq-mik.beetrack.vn/login">MÖVENPICK Resort Waverly Phu Quoc, Vietnam</a></p>
+                            <p className="pl-5">
+                                <a href="https://aeonmall.beetrack.vn/login" target="_blank">
+                                    <FontAwesomeIcon icon={faStar}/> Aeon Mall Vietnam <FontAwesomeIcon
+                                    icon={faArrowUpRightFromSquare}/>
+                                </a>
+                            </p>
+                            <p className="pl-5">
+                                <a href="https://movenpickpq-mik.beetrack.vn/login" target="_blank">
+                                    <FontAwesomeIcon icon={faStar}/> MÖVENPICK Resort Waverly Phu Quoc,
+                                    Vietnam <FontAwesomeIcon
+                                    icon={faArrowUpRightFromSquare}/>
+                                </a>
+                            </p>
                         </li>
                         <li className="pt-5">
-                            <a className="text-green-500" href="https://app.takecommandhealth.com/sign-in?next=/member-portal">
-                                Take Command Health Platform, Dalla, TX USA
+                            <a className="text-green-500"
+                               href="https://app.takecommandhealth.com/sign-in?next=/member-portal">
+                                <FontAwesomeIcon icon={faStar}/> Take Command Health Platform, Dalla, TX
+                                USA <FontAwesomeIcon
+                                icon={faArrowUpRightFromSquare}/>
                             </a>
                         </li>
                     </ul>
