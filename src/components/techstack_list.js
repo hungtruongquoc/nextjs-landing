77175217ccd81f522780399c1a14a937d09cd638
Hung Truong <hungtruongquoc@gmail.com>
2024-07-07 14:02:47 -0500

    Added loading page

--- src/components/techstack_list.js
+++ src/components/techstack_list.js
@@ -5,7 +5,7 @@ export default function TechStackList({items, title}) {
     return (
         <ul>
             {title && (<span className="font-medium capitalize">
-                <FontAwesomeIcon icon={faCircle} style={{fontSize: ".5rem"}}/>  {title}
+                <FontAwesomeIcon icon={faCircle} style={{fontSize: ".5rem"}} fixedWidth/>  {title}
             </span>)}
             {items.map((item, index) => (
                 <li key={"techstack-" + index}>{item}</li>
