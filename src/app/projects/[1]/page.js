389eb3d5d9d7f1031037a8c15933bf66966ee192
Hung Truong <hungtruongquoc@gmail.com>
2024-07-13 18:56:58 -0500

    Added new diagrams

--- src/app/projects/[1]/page.js
+++ src/app/projects/[1]/page.js
@@ -5,11 +5,18 @@ export default function ProjectPage() {
         <div className="flex flex-col gap-5 py-5">
             <h1 className="text-4xl font-black">Project Demo: React, Django, AWS</h1>
             <h2 className="text-2xl font-bold">Architecture</h2>
-            <Image src="/DemoReactDjangoApp_Architecture (1).png" alt="Architecture of the project" width={900}
+            <Image src="/DemoReactDjangoApp_Architecture.png" alt="Architecture of the project" width={900}
                    height={500}/>
             <h2 className="text-2xl font-bold">CI/CD</h2>
             <Image
                 src="/DemoReactDjangoAWS_Deployment.png"
+                width={900}
+                height={500}
+                alt="Picture of the author"
+            />
+            <h2 className="text-2xl font-bold">Entity Relationship Diagram</h2>
+            <Image
+                src="/ERD For Demo Program.png.png"
                 width={900}
                 height={500}
                 alt="Picture of the author"
