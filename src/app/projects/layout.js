7424bbcb2bf09f9a30720cb70afb1adb98759fcf
Hung Truong <hungtruongquoc@gmail.com>
2024-07-14 09:09:37 -0500

    Changed menu background color

--- src/app/projects/layout.js
+++ src/app/projects/layout.js
@@ -4,9 +4,9 @@ import {faHome} from "@fortawesome/pro-solid-svg-icons";
 export default function ProjectLayout({children}) {
     return (
         <>
-            <ul className="fixed top-0 left-0 w-full bg-white shadow-md z-10 p-4">
+            <ul className="fixed top-0 left-0 w-full shadow-md z-10 p-4 bg-blue-600">
                 <li className="container mx-auto">
-                    <Link href="/" className="text-blue-950">
+                    <Link href="/" className="text-white">
                         <FontAwesomeIcon icon={faHome}/> <span className="font-bold">Home</span>
                     </Link>
                 </li>
