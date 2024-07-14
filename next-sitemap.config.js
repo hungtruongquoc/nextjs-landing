e61a423d7c17573f739061b47062f92b66329c2f
Hung Truong <hungtruongquoc@gmail.com>
2024-07-14 07:28:49 -0500

    Fixed the page URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://www.htruong83.com/home',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
