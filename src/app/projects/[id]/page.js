a583be440f25a88448b010954959bd0bdf804515
Hung Truong <hungtruongquoc@gmail.com>
2024-07-14 11:31:27 -0500

    Convert the page into dynamic page

---
+++ src/app/projects/[id]/page.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import {documentations} from "@/data/documentations";
+
+export default function DocumentPage({ id }) {
+    const [document, setDocument] = useState(null);
+
+    useEffect(() => {
+        // Fetch the document based on the id prop
+        const selectedDocument = documentations.find(doc => doc.id === id);
+        setDocument(selectedDocument);
+    }, [id]);
+
+    if (!document) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="flex flex-col gap-5 py-5">
+            <h1 className="text-4xl font-black">{document.title}</h1>
+            {document.docs.map((doc, index) => (
+                <div key={index}>
+                    <h2 className="text-2xl font-bold">{section.doc}</h2>
+                    <Image
+                        src={doc.imageUrl}
+                        alt={doc.imageAlt}
+                        width={900}
+                        height={500}
+                    />
+                </div>
+            ))}
+        </div>
+    );
+}
---
+++ src/data/documentations.js
@@ -0,0 +1,23 @@
+export const documentations = [
+    {
+        projectId: 1,
+        title: 'Project Demo: React, Django, AWS',
+        docs: [
+            {
+                imageUrl: "/DemoReactDjangoApp_Architecture.png",
+                title: "Architecture",
+                imageAlt: "Architecture of the Demo Program",
+            },
+            {
+                imageUrl: "/DemoReactDjangoAWS_Deployment.png",
+                title: "CI/CD",
+                imageAlt: "CI/CD Pipeline for the Demo Program",
+            },
+            {
+                imageUrl: "/ERD For Demo Program.png",
+                title: "Entity Relationship Diagram",
+                imageAlt: "ERD for the Demo Program",
+            },
+        ]
+    }
+];
