0179ad87c74cd3e86163d1fb718b5382002ab739
Hung Truong <hungtruongquoc@gmail.com>
2024-07-18 08:49:24 -0500

    Added active link

--- src/components/navigation_component.js
+++ src/components/navigation_component.js
@@ -3,14 +3,28 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBars, faTimes} from "@fortawesome/pro-solid-svg-icons";
 import Link from "next/link";
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
+import {usePathname} from "next/navigation";
 
 export default function NavigationComponent() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+    const pathname = usePathname()
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
     };
+
+    const getActiveClass = useCallback((path) => {
+        if (!isMounted) return '';
+        return pathname === path ? 'border-b-2 border-white' : '';
+    }, [isMounted, pathname]);
+
+    if (!isMounted) return null; // Render nothing until mounted
 
     return (
         <nav className="bg-blue-600 shadow-md fixed w-full z-20">
@@ -28,13 +42,27 @@ export default function NavigationComponent() {
                     </div>
                     <div className={`md:flex items-center ${menuOpen ? 'block' : 'hidden'}`}>
                         <div className="flex flex-col md:flex-row md:ml-6">
-                            <Link href="/knowledge">
+                            <Link href="/architecture" passHref>
                                 <span
-                                    className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-blue-700">Knowledge</span>
+                                    className={`px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-blue-700 
+                                    focus:outline-none focus:ring-2 focus:ring-blue-500 ${getActiveClass('/architecture')}`}
+                                    aria-label="Go to Architecture page"
+                                    role="link"
+                                    tabIndex="0"
+                                >
+                                    Architecture
+                                </span>
                             </Link>
-                            <Link href="/architecture">
+                            <Link href="/knowledge" passHref>
                                 <span
-                                    className="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-blue-700">Architecture</span>
+                                    className={`px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-blue-700
+                                    focus:outline-none focus:ring-2 focus:ring-blue-500 ${getActiveClass('/knowledge')}`}
+                                    aria-label="Go to Knowledge page"
+                                    role="link"
+                                    tabIndex="0"
+                                >
+                                    Knowledge
+                                </span>
                             </Link>
                         </div>
                     </div>
