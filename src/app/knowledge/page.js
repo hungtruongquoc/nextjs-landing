950e5ea9681ccc63c2690acaf2ba441ad9d8c6e3
Hung Truong <hungtruongquoc@gmail.com>
2024-07-18 07:23:21 -0500

    Added new diagram to the web page

--- src/app/knowledge/page.js
+++ src/app/knowledge/page.js
@@ -10,6 +10,20 @@ export default function KnowledgePage() {
                 width={900}
                 height={500}
             />
+            <h2 className="text-2xl font-bold">Serverless Deployment with AWS</h2>
+            <Image
+                src="/Serverless.png"
+                alt="Serverless Deployment with AWS"
+                width={900}
+                height={500}
+            />
+            <h2 className="text-2xl font-bold">EC2 Deployment with AWS</h2>
+            <Image
+                src="/With EC2.png"
+                alt="EC2 Deployment with AWS"
+                width={900}
+                height={500}
+            />
         </div>
     );
 }
